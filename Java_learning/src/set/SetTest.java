3a53fd15aa3ae69c73eb8535ed6f2a30a9c39416
Owen Xu <869975032@qq.com>
2020-03-02 17:12:12 +0800

    Create SetTest.java
    this program uses set to print out the amount of distinct words in system.in

---
+++ Java_learning/src/set/SetTest.java
@@ -0,0 +1,32 @@
+package set;
+import java.util.*;
+/**
+ * this program uses set to print out the amount of unique words in a file
+ * @author owenxu
+ *
+ */
+
+public class SetTest {
+	public static void main(String[] args)
+	{
+		Set<String> words = new HashSet<>();//HashSet implements Set
+		long totalTime = 0;
+		
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext())
+		{
+			String word = in.next();
+			long callTime = System.currentTimeMillis();
+			words.add(word);
+			callTime = System.currentTimeMillis() - callTime;
+			totalTime += callTime;
+		}
+		
+		Iterator<String> iter = words.iterator();
+		for(int i = 0; i < 20 && iter.hasNext(); i++)
+			System.out.println(iter.next());
+		System.out.println("...");
+		System.out.println(words.size() + "unique words, " + totalTime + "miliseconds used.");
+	}
+
+}
